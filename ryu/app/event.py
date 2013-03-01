72e5cfa8499ea64ab5475cbb6409926a2cd0c28f
can <can@canx.me>
2013-03-01 10:33:51 +0800

    sample code from ntt .

--- ryu/app/event.py
+++ ryu/app/event.py
@@ -1,30 +1,32 @@
-# try out ryu's event mechanism
+import gevent
 
 from ryu.base import app_manager
-from ryu.controller import event, handler
+from ryu.controller import event
+from ryu.controller.handler import set_ev_cls
 
-class Hehe(app_manager.RyuApp):
 
-    class Event_1(event.EventBase):
-        def __init__(self):
-			super(Hehe.Event_1, self).__init__()
-            self.msg = 'event 1'
+TEST_EVENT_EV_DISPATCHER = "test_event"
 
+
+class EventTest(event.EventBase):
+    def __init__(self, msg):
+        super(EventTest, self).__init__()
+        self.msg = msg
+
+
+class TestEvent(app_manager.RyuApp):
     def __init__(self):
-        super(Hehe, self).__init__()
-#		app_manager.register_app(self)
-        self.event = Hehe.Event_1()
-        self.register_observer(Hehe.Event_1, self.name)
-        print 'my name is ', self.name
+        super(TestEvent, self).__init__()
+        self.name = 'test_event'
+        gevent.spawn_later(0, self._send_event_loop)
 
-    def ping(self):
-        self.send_event_to_observers(self.Event_1())
-        print 'pinginginging...'
+    def _send_event_loop(self):
+        i = 0
+        while True:
+            self.send_event_to_observers(EventTest(i))
+            i += 1
+            gevent.sleep(1)
 
-    @handler.set_ev_cls(Hehe.Event_1)
-    def pong(self, event):
-        print 'Got ya', event.msg
-		
-
-#h = Hehe()
-#h.ping()
+    @set_ev_cls(EventTest, TEST_EVENT_EV_DISPATCHER)
+    def _recv_handler(self, ev):
+        print 'recv:', ev.msg
